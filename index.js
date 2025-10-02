b101d92d499e6bb2eb0fdb0a21f8edb1736d41e8
laura piccioli <laura.piccioli@yahoo.com>
2025-10-02 12:45:27 -0400

    secure-the-vault

---
+++ index.js
@@ -0,0 +1,16 @@
+let code1;
+code1 = 7 + 3;
+
+let code2 = (100 - 20) / 2;
+
+let code3 = (5 % 3) + 37;
+
+let message = "The vault has been secured. The combination is:";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
